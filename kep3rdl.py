11769bb957d19b3d97c096b4655b51568692240c
partha235 <parthaa235@gmail.com>
2023-12-01 20:41:41 +0530

    working

--- kep3rdl.py
+++ kep3rdl.py
@@ -1,5 +1,11 @@
 from math import sqrt,pi
 
+def planet_vol(name:str,R):
+    volume=4*pi*(R**3)//3
+    print("{} has volume of {}".format(name,volume))
+
+# planet_vol("earth",6.371e+6)
+
 def mdv(name:str,density=None,volume=None,mass=None):
     if density is None:
         find="density"
@@ -23,6 +29,16 @@ def newton_gravity(ob1:str,ob2:str,m1,m2,r):
     G = 6.67430e-11   # gravitational constant (G)
     F = G * (m1 * m2) / r**2
     print("gravity between {} and {} is {} ".format(ob1,ob2,F))
+
+# newton_gravity("h","e",51,65,500)
+
+def escape_v(name:str,mass,r):
+    G = 6.67430e-11
+    ev = sqrt(2*G*mass/r)
+    print("escape velocity of {} is {} m/s".format(name,ev))
+
+earth_m=5.972*(10**24)
+escape_v("earth",mass=earth_m,r=6.371e+6)
 
 def planet_orbday(name:str,a:float):
     p=sqrt(a**3)
